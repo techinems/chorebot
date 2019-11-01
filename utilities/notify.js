e5b754f4b5ebb1eb20bca65de1f7fbecbc1f09b4
Zapdos26 <zapdoskid@gmail.com>
2019-11-01 18:43:55 -0400

    Removed filter since its no longer necessary here.

--- utilities/notify.js
+++ utilities/notify.js
@@ -50,7 +50,7 @@ const sendNoChores = () => postSlackMessage("No chores tonight!", [
 const postToSlack = chores => postSlackMessage("Today's chores have been posted!", [
   buildMarkdownSection(randomPhrase("greeting")),
   buildMarkdownSection("Tonight's chores:"),
-    ...chores.filter(x => x).map((c, i) => buildChoreElement(c, i)),
+  ...chores.map((c, i) => buildChoreElement(c, i)),
   buildMarkdownSection(randomPhrase("request") + " If you have any questions " +
     ":thinking_face: please reach out to the vice president!")
 ]);
